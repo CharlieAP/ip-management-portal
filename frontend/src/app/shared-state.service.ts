33c0039bc3b50dbd8b15444cf7f55b5b3d8e234f
Charlie <146344832+CharlieAP@users.noreply.github.com>
2026-04-30 19:23:26 +0100

    add shared state service to control clients and ip assets

---
+++ frontend/src/app/shared-state.service.ts
@@ -0,0 +1,34 @@
+// shared state service for sharing data across components without prop drilling
+// still hands off the data fetching to ip service to avoid coupling shared state to API related logic
+
+import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { Client, IpEntry } from "./ip.model";
+import { IpService } from "./ip.service";
+
+@Injectable({ providedIn: "root" })
+export class SharedStateService {
+	private clientsSubject = new BehaviorSubject<Client[]>([]);
+	clients$ = this.clientsSubject.asObservable();
+
+	private ipAssetsSubject = new BehaviorSubject<IpEntry[]>([]);
+	ipAssets$ = this.ipAssetsSubject.asObservable();
+
+	constructor(private ipService: IpService) {}
+
+	loadClients(): void {
+		this.ipService.getClients().subscribe({
+			next: (clients: Client[]) => this.clientsSubject.next(clients),
+			error: (err) =>
+				console.error("Unable to load clients. " + (err.message || "")),
+		});
+	}
+
+	loadIpAssets(): void {
+		this.ipService.getIpEntries().subscribe({
+			next: (ips: IpEntry[]) => this.ipAssetsSubject.next(ips),
+			error: (err) =>
+				console.error("Unable to load IP entries. " + (err.message || "")),
+		});
+	}
+}
